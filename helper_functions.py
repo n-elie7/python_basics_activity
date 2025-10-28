98582732b16bd7538b8a9813ee7003a999342d7c
Denzel_34 <d.ngabo@alustudent.com>
2025-10-28 13:57:11 +0200

    Implemented binary conversion function

--- helper_functions.py
+++ helper_functions.py
@@ -8,3 +8,14 @@ def ceate_message(name, age, name_binary, age_binary):
     message = f"Name in binary: {name_binary}\n"
     message = f"Age in binary: {age_binary}"
     return message
+
+def convert_to_binary(text):
+    if str(text).isdigit():
+        return bin(int(text))
+    else:
+        binary_list = []
+        for char in text:
+            binary_char = bin(ord(char))[2:]
+            binary_char = binary_char.zfill(8)
+            binary_list.append(binary_char)
+        return " ".join(binary_list)

e0d4037d2016cab05599779d8d17e5ca92d9d023
brina-cloud <s.kaliza1@alustudent.com>
2025-10-28 12:27:36 +0200

    greetings file

---
+++ greetings.py
@@ -0,0 +1,8 @@
+def show_intro():
+    print("=" * 30)
+    print("Welcome to the User Info Program")
+    print("=" * 30)
+
+
+def show_exit_message():
+    print("Thank you for using the program. Goodbye!")
